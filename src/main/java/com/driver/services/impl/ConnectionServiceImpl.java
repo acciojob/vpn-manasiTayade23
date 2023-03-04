0b03b518ed2819c162bb48cfd18db645898a8d03
Manasi <mansitayade23@gmail.com>
2023-03-04 17:00:06 +0530

    Solution 1

--- src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
+++ src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
@@ -21,14 +21,113 @@ public class ConnectionServiceImpl implements ConnectionService {
 
     @Override
     public User connect(int userId, String countryName) throws Exception{
+        User user = userRepository2.findById(userId).get();
+        if(user.getMaskedIp()!=null){
+           throw new Exception("Already Connected");
+       }else if(countryName.equalsIgnoreCase(user.getCountry().getCountryName().toString())){
+           return user;
+       }else{
+           if(user.getServiceProviderList() == null){
+               throw new Exception("Unable To Connect");
+           }
+           List<ServiceProvider> serviceProviderList=user.getServiceProviderList();
+           int min=Integer.MIN_VALUE;
+           Country country=null;
+           ServiceProvider serviceProvider=null;
+
+           for(ServiceProvider sp:serviceProviderList){
+               List<Country> countryList=sp.getCountryList();
+               for(Country c:countryList){
+                   if(countryName.equalsIgnoreCase(country.getCountryName().toString()) && min > sp.getId()){
+                       min=sp.getId();
+                       country=c;
+                       serviceProvider=sp;
+
+                   }
+               }
+           }
+           if(serviceProvider != null){
+               Connection connection=new Connection();
+               connection.setServiceProvider(serviceProvider);
+               connection.setUser(user);
+
+               String countryCode=country.getCode();
+               int providerId=serviceProvider.getId();
+               String masked=countryCode+providerId+userId;
+
+               user.setConnected(true);
+               user.setMaskedIp(masked);
+               user.getConnectionList().add(connection);
+
+               serviceProvider.getConnectionList().add(connection);
+               userRepository2.save(user);
+               serviceProviderRepository2.save(serviceProvider);
+
+               return user;
+           }else{
+               throw new Exception("Unable To Connect");
+           }
+       }
 
     }
     @Override
     public User disconnect(int userId) throws Exception {
+        User user =userRepository2.findById(userId).get();
+        if(!user.isConnected()){
+            throw new Exception("Disconnected");
+        }
+        user.setConnected(false);
+        user.setMaskedIp(null);
+        userRepository2.save(user);
 
+        return user;
     }
     @Override
     public User communicate(int senderId, int receiverId) throws Exception {
+        User sender=userRepository2.findById(senderId).get();
+        User receiver=userRepository2.findById(receiverId).get();
 
+        if(receiver.getMaskedIp()!=null){
+            String IP=receiver.getMaskedIp();
+            //chop
+            String code=IP.substring(0,3);
+
+            if(code.equals(sender.getCountry().getCode())) return sender;
+            else{
+                String countryName="";
+                if(code.equals(CountryName.IND.toCode())){
+                    countryName=CountryName.IND.toCode();
+                }
+                if(code.equals(CountryName.USA.toCode())){
+                    countryName=CountryName.USA.toCode();
+                }
+                if(code.equals(CountryName.JPN.toCode())){
+                    countryName=CountryName.JPN.toCode();
+                }
+                if(code.equals(CountryName.AUS.toCode())){
+                    countryName=CountryName.AUS.toCode();
+                }
+                if(code.equals(CountryName.CHI.toCode())){
+                    countryName=CountryName.CHI.toCode();
+                }
+                try{
+                    User senderUpdated=connect(senderId,countryName);
+                    return senderUpdated;
+                }catch (Exception e){
+                    throw new Exception("Error");
+                }
+            }
+        }else{
+            if(receiver.getCountry().equals(sender.getCountry())) return sender;
+            else{
+                String countryName=receiver.getCountry().getCountryName().toString();
+                try{
+                    User updateSender=connect(senderId,countryName);
+                    return updateSender;
+                }catch (Exception e){
+                    throw new Exception("Error");
+                }
+            }
+        }
     }
 }
--- src/main/java/com/driver/services/impl/UserServiceImpl.java
+++ src/main/java/com/driver/services/impl/UserServiceImpl.java
@@ -1,5 +1,9 @@
 package com.driver.services.impl;
 
+import com.driver.model.Country;
+import com.driver.model.CountryName;
+import com.driver.model.ServiceProvider;
+import com.driver.model.User;
 import com.driver.repository.CountryRepository;
 import com.driver.repository.ServiceProviderRepository;
 import com.driver.repository.UserRepository;
@@ -19,11 +23,63 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User register(String username, String password, String countryName) throws Exception{
+        if(countryName.equalsIgnoreCase("aus")||countryName.equalsIgnoreCase("ind")||countryName.equalsIgnoreCase("jpn")||
+        countryName.equalsIgnoreCase("chi")||countryName.equalsIgnoreCase("usa")){
+            //create obj
+            User user=new User();
+            //set attributes
+            user.setPassword(password);
+            user.setUsername(username);
 
+            Country country=new Country();
+
+            if(countryName.equalsIgnoreCase("ind")){
+                country.setCode(CountryName.IND.toCode());
+                country.setCountryName(CountryName.IND);
+            }
+
+            if(countryName.equalsIgnoreCase("jpn")){
+                country.setCountryName(CountryName.JPN);
+                country.setCode(CountryName.JPN.toCode());
+            }
+
+            if(countryName.equalsIgnoreCase("chi")){
+                country.setCode(CountryName.CHI.toCode());
+                country.setCountryName(CountryName.CHI);
+            }
+
+            if(countryName.equalsIgnoreCase("aus")){
+                country.setCountryName(CountryName.AUS);
+                country.setCode(CountryName.AUS.toCode());
+            }
+            if(countryName.equalsIgnoreCase("usa")){
+                country.setCode(CountryName.USA.toCode());
+                country.setCountryName(CountryName.USA);
+            }
+            //set attributes
+            country.setUser(user);
+            user.setConnected(false);
+            user.setCountry(country);
+
+            String IPAddress=country.getCode()+userRepository3.save(user).getId();
+            user.setOriginalIp(IPAddress);
+
+            userRepository3.save(user);
+            return user;
+        }else
+            throw new Exception("Not Found");
     }
 
     @Override
     public User subscribe(Integer userId, Integer serviceProviderId) {
+        User user=userRepository3.findById(userId).get();
+        ServiceProvider serviceProvider=serviceProviderRepository3.findById(serviceProviderId).get();
 
+        serviceProvider.getUserList().add(user);
+        user.getServiceProviderList().add(serviceProvider);
+
+        serviceProviderRepository3.save(serviceProvider);
+
+        return user;
     }
 }
